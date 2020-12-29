16014eccc340391b01c70bc544c93a938856cea7
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2020-12-29 03:11:40 -0300

    Consuming dice with fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './global.css';
 
 function App() {
 
-  
+  const getCars = ()=>{
+    fetch('https://api.b7web.com.br/carros/api/carros')
+      .then(function(result){
+        return result.json()
+      })
+      .then(function(json){
+        console.log('Resultado: ',json)
+      })
+  }
+
+  useEffect(()=>{
+    getCars()
+  },[])
+
 
   return (
     <div className="App">
       <h1>Olá, mundo!</h1>
+      <button onClick={getCars}>Carregar Caros</button>
     </div>
   );
 }
